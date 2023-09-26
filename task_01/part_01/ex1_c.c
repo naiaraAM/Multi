d8c03fc4c58424dba3531d512febee220ce7b36c
Naiara A <nam312@alumnos.unican.es>
2023-09-26 18:10:22 +0200

    part three of exercise one working

--- task_01/part_01/ex1_c.c
+++ task_01/part_01/ex1_c.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 
 #define NUM_MESSG 10
+#define SOURCE 1
 
 int main(int argc, char* argv[]) {
     int rank; // rank of process
     int num_procs; // number of processes
-    int source;
+    int source = SOURCE;
     int destination = 0;
     MPI_Status status;
     int index = 0;
@@ -20,20 +21,18 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     int recv_buff[NUM_MESSG * (num_procs - 1)];
-    
 
     if (rank != 0) { // multiple senders
-        for (int tag = 0; tag < NUM_MESSG; tag++) {
-            MPI_Send(&send_buff[tag], 1, MPI_INT, destination, tag, MPI_COMM_WORLD);
-            printf("SENDING... Content of the message: %d\n", send_buff[tag]);
+        for (int tag = 1; tag <= NUM_MESSG; tag++) {
+            MPI_Send(&send_buff[tag - 1], 1, MPI_INT, destination, tag, MPI_COMM_WORLD);
+            printf("SENDING... Content of the message: %d from source %d\n", send_buff[tag - 1], rank);
         }
 
-    } else if (rank == 0) { // receiver 
-        for (int tag = 0; tag < NUM_MESSG * (num_procs - 1); tag++) {
-            MPI_Recv(&recv_buff[tag], 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            printf("RECEIVING... Content of the message: %d from source %d\n", recv_buff[tag], status.MPI_SOURCE);
+    } else if (rank == 0) { // receiver
+        for (int tag = NUM_MESSG; tag > 0; tag--) {
+            MPI_Recv(&recv_buff[tag - 1], 1, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+            printf("RECEIVING... Content of the message: %d from source %d\n", recv_buff[tag - 1], status.MPI_SOURCE);
         }
-
     }
     MPI_Finalize();
     return EXIT_SUCCESS;
